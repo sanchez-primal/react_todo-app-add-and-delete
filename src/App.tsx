d2382667125c843662ecd13570f81ccbfd4542a9
Alex Kh. <sanchez.primal@gmail.com>
2026-04-21 14:01:38 +0200

    import the latest project version

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,117 @@
-/* eslint-disable max-len */
-/* eslint-disable jsx-a11y/control-has-associated-label */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { UserWarning } from './UserWarning';
+import { getTodos, USER_ID } from './api/todos';
 
-const USER_ID = 0;
+import {
+  ErrorNotification,
+  ErrorMessages,
+} from './components/ErrorNotification';
+import { Footer, TodoStatus } from './components/Footer';
+import { Header } from './components/Header';
+import { Todo } from './types/Todo';
+import { Todo as TodoItem } from './components/Todo';
 
 export const App: React.FC = () => {
+  const [todos, setTodos] = useState<Todo[]>([]);
+  const [filteringByCompleted, setFilteringByCompleted] = useState<TodoStatus>(
+    TodoStatus.ALL,
+  );
+  const [errorMessage, setErrorMessage] = useState(ErrorMessages.NONE);
+  const [errorRenderIteration, setErrorRenderIteration] = useState(1);
+
+  function displayError(message: ErrorMessages) {
+    setErrorMessage(message);
+    setErrorRenderIteration(current => current + 1);
+  }
+
+  // > Fetching
+
+  useEffect(() => {
+    getTodos()
+      .then(setTodos)
+      .catch(() => {
+        displayError(ErrorMessages.FAILED_LOAD);
+      });
+  }, []);
+
+  // > Filtering
+
+  const filteredTodos = todos.filter(todo => {
+    let satisfiesCompleted: boolean;
+
+    switch (filteringByCompleted) {
+      case TodoStatus.ACTIVE:
+        satisfiesCompleted = !todo.completed;
+        break;
+      case TodoStatus.COMPLETED:
+        satisfiesCompleted = todo.completed;
+        break;
+      default:
+        satisfiesCompleted = true;
+        break;
+    }
+
+    return satisfiesCompleted;
+  });
+
+  // > Counting incomplete
+
+  let incompleteTodoQuantity = 0;
+
+  todos.forEach(todo => {
+    if (!todo.completed) {
+      incompleteTodoQuantity += 1;
+    }
+  });
+
+  const hasCompletedTodos = todos.length !== incompleteTodoQuantity;
+
+  // > Returns
+
   if (!USER_ID) {
     return <UserWarning />;
   }
 
   return (
-    <section className="section container">
-      <p className="title is-4">
-        Copy all you need from the prev task:
-        <br />
-        <a href="https://github.com/mate-academy/react_todo-app-loading-todos#react-todo-app-load-todos">
-          React Todo App - Load Todos
-        </a>
-      </p>
+    <div className="todoapp">
+      <h1 className="todoapp__title">todos</h1>
 
-      <p className="subtitle">Styles are already copied</p>
-    </section>
+      <div className="todoapp__content">
+        <Header />
+
+        {/*
+          Why hide this if it doesn't take up any space?
+          Wouldn't it be worse for performance?
+        */}
+        {!!todos.length && (
+          <section className="todoapp__main" data-cy="TodoList">
+            {filteredTodos.map(todo => {
+              return (
+                <TodoItem
+                  key={todo.id}
+                  todo={todo}
+                  isSelected={false}
+                  isLoading={false}
+                />
+              );
+            })}
+          </section>
+        )}
+
+        {!!todos.length && (
+          <Footer
+            incompleteTodoQuantity={incompleteTodoQuantity}
+            onFilterSelect={setFilteringByCompleted}
+            activeFiltering={filteringByCompleted}
+            isAnyTodoCompleted={hasCompletedTodos}
+          />
+        )}
+      </div>
+
+      <ErrorNotification
+        errorMessage={errorMessage}
+        key={errorRenderIteration}
+      />
+    </div>
   );
 };
